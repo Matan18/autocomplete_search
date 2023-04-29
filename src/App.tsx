de03155e64e69bdf087a30c794c49d4272c922c6
Mateus Andriola <mateusandriola18@gmail.com>
2023-04-29 12:10:13 -0300

    feat(Footer component)

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "./Footer";
 import { Searchbar } from "./Searchbar";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         </h3>
       </div>
       <Searchbar />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer/index.tsx
@@ -0,0 +1,24 @@
+export const Footer = () => (
+  <footer className="flex flex-col items-center gap-1 fixed bottom-2">
+    <small className="text-zinc-400">
+      Project created by:{" "}
+      <a
+        href="https://github.com/Matan18"
+        target="_blank"
+        className="hover:text-zinc-300"
+      >
+        Mateus Andriola
+      </a>
+    </small>
+    <small className="text-zinc-400">
+      Repo:{" "}
+      <a
+        href="https://github.com/Matan18/autocomplete_search"
+        target="_blank"
+        className="hover:text-zinc-300"
+      >
+        autocomplete_search
+      </a>
+    </small>
+  </footer>
+);
